54f4de7c6f612687fb9345e950125658ed7c5bad
congquynguyen296 <congquynguyen296@gmail.com>
2025-05-02 20:50:46 +0700

    Chức năng: Xây dựng chức năng xác thực và phân quyền - Subtask: Triển khai JWT và OAuth

--- hotelmanagement/src/main/java/com/dashotel/hotelmanagement/configuration/PasswordEncoderConfig.java
+++ hotelmanagement/src/main/java/com/dashotel/hotelmanagement/configuration/PasswordEncoderConfig.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+
+// Config bean password
 @Configuration
 public class PasswordEncoderConfig {
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
