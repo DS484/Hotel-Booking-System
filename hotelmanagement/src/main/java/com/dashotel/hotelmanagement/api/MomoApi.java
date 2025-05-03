32d9da4be479180eb2d746b646fab79cdd512df4
DucSang404 <ndsang404@gmail.com>
2025-05-03 11:00:57 +0700

    new

--- hotelmanagement/src/main/java/com/dashotel/hotelmanagement/api/MomoApi.java
+++ hotelmanagement/src/main/java/com/dashotel/hotelmanagement/api/MomoApi.java
@@ -2,12 +2,14 @@ package com.dashotel.hotelmanagement.api;
 
 import com.dashotel.hotelmanagement.dto.request.CreateMonoRequest;
 import com.dashotel.hotelmanagement.dto.response.CreateMonoResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-@FeignClient(name = "momo", url = "${momo.end-point}")
+@Component
 public interface MomoApi {
     @PostMapping("/create")
     CreateMonoResponse createMomoQR(@RequestBody CreateMonoRequest createMonoRequest);
