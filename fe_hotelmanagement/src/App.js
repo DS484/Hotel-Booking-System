732f809e3e3ff9e78ca956a4eae32fc2ee104898
t9tieanh <phama9162@gmail.com>
2025-05-04 22:45:13 +0700

    push origin main

--- fe_hotelmanagement/src/App.js
+++ fe_hotelmanagement/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
         position="top-right"
         autoClose={3000}
         style={{ height: '100px', width: '400px', marginTop: '100px' }}
-        hideProgressBar={true}
+        hideProgressBar={false}
         newestOnTop={false}
         closeOnClick={false}
         rtl={false}
